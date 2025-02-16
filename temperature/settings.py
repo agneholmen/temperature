1dd5f2cf89a4803bab3dee930ae7a18775cf2aa6
Emil Agneholm <emil.agneholm@gmail.com>
2025-02-16 10:34:19 +0100

    Updated ALLOWED_HOSTS.

--- temperature/settings.py
+++ temperature/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-u1j35+zpaj5u^wi21bvo%eix1h@k%dfy)_a9u$xgp&h--g)*^(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
