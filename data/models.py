98184dafe85b34aacd242ae3f3d06ba268719c7e
Emil Agneholm <emil.agneholm@gmail.com>
2025-02-23 19:14:07 +0100

    Changed point name to unique.

--- data/models.py
+++ data/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Point(models.Model):
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=20, unique=True)
     description = models.TextField(blank=True)
     
     def __str__(self):
