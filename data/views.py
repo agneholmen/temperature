7674b19114b242ba07aa48d106387be83bec07bd
Emil Agneholm <emil.agneholm@gmail.com>
2025-03-01 20:08:44 +0100

    Added table with latest measurement for each point on statistics.html.

--- data/views.py
+++ data/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
+from .models import Measurement, Point
 
 def home(request):
     return render(
@@ -8,7 +10,24 @@ def home(request):
     )
 
 def statistics(request):
+    temperatures = get_latest_temperatures()
+
     return render(
         request,
-        'data/statistics.html'
+        'data/statistics.html',
+        {'temperatures': temperatures}
     )
+
+def get_latest_temperatures():
+    temperatures = []
+
+    for point in Point.objects.all():
+        if Measurement.objects.filter(point=point).exists():
+            measurement = Measurement.objects.filter(point=point).latest('time')
+            temperatures.append({
+                            'temperature': measurement.temperature, 
+                            'time': measurement.time,
+                            'point': point.name
+                            })
+
+    return temperatures
